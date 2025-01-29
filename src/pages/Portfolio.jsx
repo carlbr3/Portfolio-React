e709a8fdff74f01de789e1f8c5184f91b7b7200e
carlbr3 <brandon.carlson2089@gmail.com>
2025-01-28 21:58:55 -0700

    updatedgithublinks

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -11,41 +11,41 @@ const Portfolio = () => {
             description: "This project was created to be able to track the timing for each page that is being read on a script for a stagehand",
             image: "assets/img/Page Timings.png",
             deployedlink: "https://carlbr3.github.io/Page-Timings/",
-            repository: "https://github.com/carlbr3/Page-Timings"
+            repoLink: "https://github.com/carlbr3/Page-Timings"
         },
         {
             title: "README-Generator",
             description: "This repository is where I developed a command line application to quickly create a professional README for new projects.",
             image: "./assets/img/README Generator.png",
             deployedvideo: "https://drive.google.com/file/d/10dZTy39GpLwUWAY-XnTRNkHNeLDh7fif/view",
-            repository: "https://github.com/carlbr3/README-Generator"
+            repoLink: "https://github.com/carlbr3/README-Generator"
         },
         {
             title: "Vehicle-Build-Test",
             description: "The purpose of building this application was to demonstrate OOP (Object Oriented Programming) and show how different classes function.",
             image: "./assets/img/Vehicle-Build-Test.png",
             deployedvideo: "https://drive.google.com/file/d/1WwAg_R4XFckuzblO1gcSrXEf-EmOKiFX/view",
-            repository: "https://github.com/carlbr3/Vehicle-Build-Test"
+            repoLink: "https://github.com/carlbr3/Vehicle-Build-Test"
         },
         {
             title: "Weather-Dashboard-Forecast",
             description: "The purpose of this application was to build a weather dashboard that calls the OpenWeather API, then renders the data in the browser.",
             image: "https://via.placeholder.com/150",
             deployedvideo: "https://www.youtube.com/watch?v=Q8v3h3Y2J1c",
-            repository: "https://github.com/carlbr3/Weather-Dashboard-Forecast"
+            repoLink: "https://github.com/carlbr3/Weather-Dashboard-Forecast"
         },
         {
             title: "Employee-Tracker",
             description: "Employee Tracking Database for Front End",
             image: "https://via.placeholder.com/150",
             deployedvideo: "https://www.youtube.com/watch?v=Q8v3h3Y2J1c",
-            repository: "https://github.com/carlbr3/Employee-Tracker"
+            repoLink: "https://github.com/carlbr3/Employee-Tracker"
         },
         {
             title: "Simple HTML/CSS",
             description: "Mini project to show off HTML and CSS skills",
             image: "assets/img/HTML:CSS.png",
-            repository: "https://github.com/carlbr3/module-2-mini-project"
+            repoLink: "https://github.com/carlbr3/module-2-mini-project"
         }
     ];
     //Add the upcoming projects to the Project component
@@ -59,7 +59,9 @@ const Portfolio = () => {
                     title={project.title} 
                     description={project.description} 
                     image={project.image} 
-                    repository={project.repository} />
+                    deployedlink={project.deployedlink}
+                    deployedvideo={project.deployedvideo}
+                    repoLink={project.repoLink} />
                 ))}
             </div>
         </div>
